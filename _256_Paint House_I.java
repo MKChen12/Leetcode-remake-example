e017d9f9da6d291822edca4c881215bf61a614f0
ShangrouS <35820840+ShangrouS@users.noreply.github.com>
2018-01-27 21:56:58 -0800

    Update _256_Paint House_I.java

--- _256_Paint House_I.java
+++ _256_Paint House_I.java
@@ -29,7 +29,7 @@ public class PaintHouse {
      * @return
      */
     public int minCost(int[][] costs) {
-        if (costs == null || costs[0].length == 0) return 0;
+        if (costs == null || costs[0].length == 0) return 0;  // costs[0].length -lc报错 
         for (int i = 1; i < costs.length; i++) {
             costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
             costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
